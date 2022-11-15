6209eb7e953a4f15ee5850a9df2510b9a3cb8078
daviniathebridge <davinia@thebridgeschool.es>
2022-11-15 17:48:41 +0100

    Davinia

--- dataBase/mongo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const url = "mongodb://localhost:27017/jwt";
-
-mongoose.connect(url, function (err) {
-  if (err) throw err;
-  console.log("BBDD ON");
-});
-
